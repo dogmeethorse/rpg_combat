807ef4e96b93ecc32e1fbe364a60bc2542739890
Sam <dogmeethorse@yahoo.com>
2013-09-25 12:01:55 -0400

    changed css a little

--- RPG_combat.js
+++ RPG_combat.js
@@ -1,11 +1,22 @@
 var slime1hp = 10;
 var outputElement = document.getElementById('output');
 
-function sendOutput(output, addOrReplace){
-   	if(addOrReplace == 'replace'){
+function randomInt(min,max){
+            return Math.floor(Math.random() * (max - (min-1) )) + min;
+        }
+        
+function sendMessage(output,replace){
+   	if(addOrReplace == true){
        	outputElement.innerHTML = '<p>' + output + '</p>';
     }
-   	if(addOrReplace == 'add'){
+   	else{
         outputElement.innerHTML += '<p>' + output + '</p>';
     }
 }
+
+function Game_Entity(hp,dmg){
+	this.dmg = 1;
+	this.hp = 10;
+}
+
+hero = new Game_Entity(10,1);
