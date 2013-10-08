1de9f2405dc709d252eea3f7184a4ca304ad6ca2
Sam <dogmeethorse@yahoo.com>
2013-10-08 19:00:01 -0400

    starting to set the framework to make the game responsive for testing

--- RPG_combat.js
+++ RPG_combat.js
@@ -1,10 +1,18 @@
-
+/* We now have a current enemy and have created a slime enemy
+ * the next step is to make it so that the message box sends the user input so that we can test to see if the 
+ * program is functioning properly so that we can debug.
+ * Now I added some sample dialog and a name prop to enemy.
+ * GET ATTACKS WORKING THEN ADD GAME STATES.
+ */
 var dragon_screen = document.getElementById('game_screen');
 var d_ctx = dragon_screen.getContext('2d');
 
 var outputElement = document.getElementById('output');
 var statOutputEl = document.getElementById('stats');
 var messageEl = document.getElementById('message_box');
+
+var combat_happening = true;// should start the game as false
+var current_enemy = null;
 
 function randomInt(min,max){
             return Math.floor(Math.random() * (max - (min-1) )) + min;
@@ -23,17 +31,26 @@ function setStats(){
 	statOutputEl.innerHTML = "<p class='game'> HP: " + hero.hp +"<br>XP: " + hero.xp + "<br> LVL: " + hero.lvl+ "<br> GLD: " + hero.gld + "</p>";
 }
 
+function endCombat(){
+
+}
+
 function Game_Entity(hp,dmg){
 	this.maxDmg = dmg;
 	this.minDmg = 1;
 	this.hp = hp;
 	this.atkChance = 50;
 	this.alive = true;
+	this.escChance = 50;
 	
 	Game_Entity.prototype.attack = function(target){
 		if(atkChance >= randomInt(1,100)){
 			attackDmg = randomInt(this.minDmg,this.maxDmg);
 			target.hp -= attackDmg;
+			return attackDmg;
+		}
+		else{
+			return false;
 		}
 	}
 	Game_Entity.prototype.isAlive = function(){
@@ -44,19 +61,61 @@ function Game_Entity(hp,dmg){
 			return true;
 		}
 	}
+	Game_Entity.prototype.run = function(){
+		if(randomInt(1,50)< this.escChance){
+			
+		}
+	
+	}
 }
 
+
+
+function Enemy(hp,dmg,aggro,atk,esc, name){
+	this.name = name;
+	this.atkChance = atk;
+	this.escChance = esc;
+	this.aggro = aggro;  //chance on whether monster will attack of flee from 0-1
+	var maxHp = this.hp;
+	
+	Enemy.prototype.takeTurn = function(){
+		if(1-this.hp/maxhp >aggro){
+			if(Math.random()> aggro){
+				this.run();
+				return;
+			}
+		}
+		this.attack();
+	}
+}
+Enemy.prototype= new Game_Entity();
+Enemy.prototype.constructor = Enemy();
+
+
 function handle(action){
-	switch(action){
-	case 'attack':
-		hero.attack();
-	break;
-	case 'defend':
-	break;
-	case 'item':
-	break;
-	case 'run':
-	break;
+	console.log("event function triggering");
+	if(combat_happening){
+		switch(action){
+			case 'attack':
+				var result = hero.attack(current_enemy);
+			break;
+			case 'defend':
+			break;
+			case 'item':
+			break;
+			case 'run':
+			break;
+		}
+	}
+	if(result!= null){
+		sendMessage("You hit the " + current_enemy + "for" + result + "damage! +<br>", true);
+	}
+	
+	if(current_enemy.isAlive()){
+		current_enemy.attack(hero);
+	}
+	else{
+		sendMessage("You have defeated the "+ current_enemy.name + " Congratulations!", false);
 	}
 	setStats();
 }
@@ -66,3 +125,6 @@ hero.xp = 0;
 hero.lvl = 1;
 hero.gld = 0;
 setStats();
+
+slime = new Enemy(3,1,0,0, 'slime');
+current_enemy = slime;
