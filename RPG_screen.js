a37b6ecb64bad0a86b6daa011ed81369f8b80b02
Sam <dogmeethorse@yahoo.com>
2013-11-15 10:06:38 -0500

    shaking

--- RPG_screen.js
+++ RPG_screen.js
@@ -1,6 +1,7 @@
 /*
  * functions and objects for drawing things and visual effects.
  */
+var game_box = {}
 
 var fightArea = {};
 
@@ -11,10 +12,36 @@ fightArea.draw = function(){
 	d_ctx.drawImage(fightArea.img, -10,-10, 420, 420);
 }
 
+fightArea.flash = function(){
 
+}
 
-function shakeGame(){
-	var moveLeftX = '5px';
-	var moveRightX = '-5px';
-	background.setAttrubute();
+game_box.shake = function(){
+	game_box.shakeStart = 0;
+	game_box.shakePos = 0;
+ 	game_box.counter = 0;
+ 	game_box.margin = 0;
+	game_box.increment = 1;
+	game_box.shakeAmt = 50;
+	
+	function doShake(){
+		console.log("shaking shakePos =" + game_box.shakePos);
+		if (game_box.shakePos >= game_box.shakeStart) {
+			game_box.shakePos = game_box.shakeStart - game_box.shakeAmt;
+			background.style.marginLeft =  game_box.shakePos +"px";
+		}
+		else{
+			shakePos = shakeStart + (2*shakeAmt);
+			background.style.marginLeft = game_box.shakePos  + "px";
+			game_box.shakeAmt -= 50*(game_box.shakeAmt/game_box.shakeStart);
+		}
+		if (game_box.shakeAmt > 2){
+			setTimeout(doShake, game_box.shakeAmt);
+		}
+		else{
+			background.style.marginLeft = game_box.shakeStart  + "px";
+		}
+	}	
+	
+	doShake();
 }
